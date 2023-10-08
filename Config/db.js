5675a97083dc0ac28f8cdff8917daa2aa38c2193
Shashwat Mahendra <shashwatmahender2104@gmail.com>
2023-10-08 21:05:26 +0530

    Debugged

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// const connection = mongoose.connect(process.env.mongourl);
-const connection = mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.qoffovv.mongodb.net/task?retryWrites=true&w=majority"
-);
+const connection = mongoose.connect(process.env.MONGO_URL);
+// const connection = mongoose.connect(
+//   "mongodb+srv://admin:admin@cluster0.qoffovv.mongodb.net/task?retryWrites=true&w=majority"
+// );
 
 module.exports = { connection };
